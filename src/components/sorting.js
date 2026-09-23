fb507ca6b445ecd18d71ffe06fba4da949e65a46
Anelia <aneliapartsikova4000@gmail.com>
2026-09-23 23:36:20 +0300

    сделала проект

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,18 +1,30 @@
 import {sortCollection, sortMap} from "../lib/sort.js";
 
 export function initSorting(columns) {
+    let field = null;
+    let order = 'none';
+
     return (data, state, action) => {
-        let field = null;
-        let order = null;
 
-        if (action && action.name === 'sort') {
-            // @todo: #3.1 — запомнить выбранный режим сортировки
+        if (action && columns.includes(action)) {
+            action.dataset.value = sortMap[action.dataset.value];
+            field = action.dataset.field;
+            order = action.dataset.value;
 
-            // @todo: #3.2 — сбросить сортировки остальных колонок
-        } else {
-            // @todo: #3.3 — получить выбранный режим сортировки
+            columns.forEach(column => {
+                if (column.dataset.field !== action.dataset.field) {
+                    column.dataset.value = 'none';
+                }
+            });
         }
 
+        columns.forEach(column => {
+            if (column.dataset.value !== 'none') {
+                field = column.dataset.field;
+                order = column.dataset.value;
+            }
+        });
+
         return sortCollection(data, field, order);
-    }
+    };
 }
